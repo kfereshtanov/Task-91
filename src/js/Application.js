6448c4ad2141f05c6c7eb835ae8cca6465e72942
kfereshtanov <kfereshtanov@2create.studio>
2023-01-31 23:21:33 +0200

    Add progress

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,24 +2,36 @@ import EventEmitter from "eventemitter3";
 import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+	static get events() {
+		return {
+			READY: "ready",
+		};
+	}
 
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
+		const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+		let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+		this._beat = new Beat();
 
-    document.querySelector(".main").appendChild(message);
+		this._beat.on(Beat.events.BIT, () => {
+				if (count < lyrics.length) {
+					this._create();
+					count++;
+				}
 
-    this.emit(Application.events.READY);
-  }
+				return;
+		})
+
+		this._create = () => {
+			const message = document.createElement("div");
+			message.classList.add("message");
+			message.innerText = `${lyrics[count]}`;
+			document.querySelector(".main").appendChild(message);
+		}
+
+		this.emit(Application.events.READY);
+	}
 }
